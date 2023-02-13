17d50b60edc9dd34e7bbcfe9d4ab2d721dca11fe
Raghav Luthra <raghavluthra1@gmail.com>
2023-02-13 13:34:36 +0530

    connected mongodb

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import connectMongodb from "@/lib/mongodb";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const db = await connectMongodb();
+  console.log("db =>", db);
+  res.status(200).json({ name: "John Doe" });
 }
