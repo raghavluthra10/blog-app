bd408d7a82cb90652de7e337b791d0549b776ef4
Raghav Luthra <raghavluthra1@gmail.com>
2023-02-13 17:01:30 +0530

    updated mongodb connection config

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,20 @@
 import connectMongodb from "@/lib/mongodb";
+import User from "@/models/userModel";
 
 export default async function handler(req, res) {
-  const db = await connectMongodb();
-  console.log("db =>", db);
-  res.status(200).json({ name: "John Doe" });
+  try {
+    if (req.method === "GET") {
+      await connectMongodb();
+
+      const user = await User.find({});
+
+      console.log("user", user);
+
+      return res.status(200).json({ data: user });
+    }
+    return res.status(200).json({ success: true });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal server error!");
+  }
 }
