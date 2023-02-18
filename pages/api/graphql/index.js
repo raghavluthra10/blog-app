000bf379ec90f7915fbc1efd7d5afc8d0554165e
Raghav Luthra <raghavluthra1@gmail.com>
2023-02-18 20:34:13 +0530

    update: initialising DB connection before graphql server

--- pages/api/graphql/index.js
+++ pages/api/graphql/index.js
@@ -2,17 +2,21 @@ import resolvers from "@/graphql/resolvers";
 import typeDefs from "@/graphql/typeDefs";
 import connectMongodb from "@/lib/mongodb";
 import { createYoga, createSchema } from "graphql-yoga";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
 
 const schema = createSchema({
   typeDefs: typeDefs,
   resolvers: resolvers,
 });
 
+connectMongodb();
+
 export default createYoga({
   schema,
-  context: () => {
-    connectMongodb();
-    return {};
+  context: async ({ req, res }) => {
+    const session = await getServerSession(req, res, authOptions);
+    return { session };
   },
   graphqlEndpoint: "/api/graphql",
 });
